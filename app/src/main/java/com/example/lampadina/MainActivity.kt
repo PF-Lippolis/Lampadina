2e7527544be79498497deeaa996ffa3849417b37
P.F. Lippolis <pf.lippolis@gmail.com>
2021-10-07 15:49:46 +0200

    Add lightbulb behaviour

--- app/src/main/java/com/example/lampadina/MainActivity.kt
+++ app/src/main/java/com/example/lampadina/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.lampadina
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CompoundButton
 import com.example.lampadina.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +15,18 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSwitchListener()
+    }
 
+    private fun setSwitchListener() {
+        binding.lightSwitch.setOnCheckedChangeListener { _, isChecked ->
+            if(isChecked) {
+                binding.imageOn.visibility = View.VISIBLE
+                binding.imageOff.visibility = View.INVISIBLE
+            } else {
+                binding.imageOn.visibility = View.INVISIBLE
+                binding.imageOff.visibility = View.VISIBLE
+            }
+        }
     }
 }
